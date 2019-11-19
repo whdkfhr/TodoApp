3d35c1f0fdbca6e3bd45a7fa457efdd37fbd54ae
whdkfhr <whwoals18@naver.com>
2019-11-19 19:04:45 +0900

    add db and rest class

--- server/src/main/java/com/arok/springbootvue/RestRepositoryConfigurator.java
+++ server/src/main/java/com/arok/springbootvue/RestRepositoryConfigurator.java
@@ -1,4 +1,13 @@
 package com.arok.springbootvue;
 
-public class RestRepositoryConfigurator {
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestRepositoryConfigurator implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Todo.class);
+    }
 }
