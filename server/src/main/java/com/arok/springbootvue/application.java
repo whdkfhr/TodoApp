f7161f4b9da0361602d853b2f029f7cddf81aa48
whdkfhr <whwoals18@naver.com>
2020-04-15 15:40:06 +0900

    Add a Vue config file

--- server/src/main/java/com/arok/springbootvue/application.java
+++ server/src/main/java/com/arok/springbootvue/application.java
@@ -32,12 +32,14 @@ public class application {
         };
     }
 
+    // fix the CORS errors
     @Bean
     public FilterRegistrationBean simpleCorsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
 
+        /* URL below needs to match the Vue client URL and port */
         config.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
         config.setAllowedMethods(Collections.singletonList("*"));
         config.setAllowedHeaders(Collections.singletonList("*"));
