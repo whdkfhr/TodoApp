eab40f95458c48e00db3e6d037c529adc5703381
whdkfhr <whwoals18@naver.com>
2020-04-18 17:44:37 +0900

    create a Vue component

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 
 Vue.config.productionTip = false
 
+import VueLogger from 'vuejs-logger';
+
+const options = {
+  isEnabled: true,
+  logLevel : 'debug',
+  stringifyArguments : false,
+  showLogLevel : true,
+  showMethodName : false,
+  separator: '|',
+  showConsoleColors: true
+};
+
+Vue.use(VueLogger, options);
+
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+});
