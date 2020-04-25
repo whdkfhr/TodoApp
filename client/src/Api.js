a6f70ae860ca821e4e754e909d38e3479bed1bb2
whdkfhr <whwoals18@naver.com>
2020-04-25 15:54:28 +0900

    Finish the CRUD Methods

--- client/src/Api.js
+++ client/src/Api.js
@@ -22,3 +22,4 @@ export default {
   // (D)elete
   removeForId: (id) => instance.delete('todos/'+id)
 }
+
--- client/src/components/Todos.vue
+++ client/src/components/Todos.vue
@@ -144,75 +144,90 @@
     // methods that implement data logic.
     // note there's no DOM manipulation here at all.
     methods: {
-
-      addTodo: function () {
-        var value = this.newTodo && this.newTodo.trim()
-        if (!value) {
-          return
-        }
-
-        this.todos.push({
-          title: value,
-          completed: false
-        });
-
-        this.newTodo = ''
-      },
-
-      setVisibility: function(vis) {
-        this.visibility = vis
-      },
-
-      completeTodo (todo) {
-      },
-
-      removeTodo: function (todo) { // notice NOT using "=>" syntax
-        this.todos.splice(this.todos.indexOf(todo), 1)
-      },
-
-      editTodo: function (todo) {
-        this.beforeEditCache = todo.title
-        this.editedTodo = todo
-      },
-
-      doneEdit: function (todo) {
-        if (!this.editedTodo) {
-          return
-        }
-
-        this.editedTodo = null
-        todo.title = todo.title.trim()
-
-        if (!todo.title) {
-          this.removeTodo(todo)
-        }
-      },
-
-      cancelEdit: function (todo) {
-        this.editedTodo = null
-        todo.title = this.beforeEditCache
-      },
-
-      removeCompleted: function () {
-        this.todos = filters.active(this.todos)
-      },
-
-      handleErrorClick: function () {
-        this.error = null;
-      },
-    },
-
-    // a custom directive to wait for the DOM to be updated
-    // before focusing on the input field.
-    // http://vuejs.org/guide/custom-directive.html
-    directives: {
-      'todo-focus': function (el, binding) {
-        if (binding.value) {
-          el.focus()
+          addTodo: function () {
+            var value = this.newTodo && this.newTodo.trim()
+            if (!value) {
+              return
+            }
+            api.createNew(value, false).then( (response) => {
+              this.$log.debug("New item created:", response);
+              this.todos.push({
+                id: response.data.id,
+                title: value,
+                completed: false
+              })
+            }).catch((error) => {
+              this.$log.debug(error);
+              this.error = "Failed to add todo"
+            });
+            this.newTodo = ''
+          },
+          setVisibility: function(vis) {
+            this.visibility = vis
+          },
+          completeTodo (todo) {
+            api.updateForId(todo.id, todo.title, todo.completed).then((response) => {
+              this.$log.info("Item updated:", response.data);
+            }).catch((error) => {
+              this.$log.debug(error)
+              todo.completed = !todo.completed
+              this.error = "Failed to update todo"
+            });
+          },
+          removeTodo: function (todo) { // notice NOT using "=>" syntax
+            api.removeForId(todo.id).then(() => { // notice AM using "=>" syntax
+              this.$log.debug("Item removed:", todo);
+              this.todos.splice(this.todos.indexOf(todo), 1)
+            }).catch((error) => {
+              this.$log.debug(error);
+              this.error = "Failed to remove todo"
+            });
+          },
+          editTodo: function (todo) {
+            this.beforeEditCache = todo.title
+            this.editedTodo = todo
+          },
+          doneEdit: function (todo) {
+            if (!this.editedTodo) {
+              return
+            }
+            this.$log.info("Item updated:", todo);
+            api.updateForId(todo.id, todo.title.trim(), todo.completed).then((response) => {
+              this.$log.info("Item updated:", response.data);
+              this.editedTodo = null
+              todo.title = todo.title.trim()
+            }).catch((error) => {
+              this.$log.debug(error)
+              this.cancelEdit(todo)
+              this.error = "Failed to update todo"
+            });
+            if (!todo.title) {
+              this.removeTodo(todo)
+            }
+          },
+          cancelEdit: function (todo) {
+            this.editedTodo = null
+            todo.title = this.beforeEditCache
+          },
+          removeCompleted: function () {
+            this.todos = filters.active(this.todos)
+          },
+          handleErrorClick: function () {
+            this.error = null;
+          },
+        },
+        // a custom directive to wait for the DOM to be updated
+        // before focusing on the input field.
+        // http://vuejs.org/guide/custom-directive.html
+        directives: {
+          'todo-focus': function (el, binding) {
+            if (binding.value) {
+              el.focus()
+            }
+          }
         }
       }
-    }
-  }
+
 
   export default Todos
 </script>
